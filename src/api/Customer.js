e13381a211b79deff4aff7133ddc7b0159a91896
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-05-14 21:26:08 +0530

    feat: customer get

--- src/api/Customer.js
+++ src/api/Customer.js
@@ -15,10 +15,28 @@ export function saveCustomer(customer, onSuccess, onError){
     })
 }
 
-// export function getAllCustomers(onSuccess, onError){
-//     $.ajax({
-//         type: 'GET',
-//         url: CUSTOMER_URL,
-//
-//     })
-// }
+export function getAllCustomers(onSuccess, onError){
+    $.ajax({
+        type: 'GET',
+        url: CUSTOMER_URL,
+        success: function (customers){
+            onSuccess(customers);
+        },
+        error: function (err){
+            onError(err);
+        }
+    })
+}
+
+export function getCustomerById(customerId, onSuccess, onError){
+    $.ajax({
+        type: 'GET',
+        url: `${CUSTOMER_URL}/${customerId}`,
+        success: function (customer){
+            onSuccess(customer);
+        },
+        error: function (err){
+            onError(err);
+        }
+    })
+}
--- src/controller/customerController.js
+++ src/controller/customerController.js
@@ -1,87 +1,153 @@
 import validator from "validator/es";
 import CustomerModel from "../model/CustomerModel.js";
-import {saveCustomer} from "../api/Customer.js";
+import {getAllCustomers, getCustomerById, saveCustomer} from "../api/Customer.js";
 
-$(document).ready(function() {
+const customerName = $('#customer_name');
+const customerAddress = $('#customer_address');
+const customerContact = $('#customer_contact');
+const customerEmail = $('#customer_email');
+const customerDob = $('#customer_dob');
 
-    const customerName = $('#customer_name');
-    const customerAddress = $('#customer_address');
-    const customerContact = $('#customer_contact');
-    const customerEmail = $('#customer_email');
-    const customerDob = $('#customer_dob');
+function loadAllCustomers() {
+    getAllCustomers(
+        function (customers) {
+            $('#customer_table tbody').empty();
+            customers.map((customer, index) => {
+                let rowHtml =
+                    `<tr>
+                            <th>${index + 1}</th>
+                            <td>${customer.name}</td>
+                            <td>${customer.email}</td>
+                            <td>${customer.contact}</td>  
+                            <td>${customer.address}</td>  
+                            <td>${customer.joinedDate}</td>  
+                            <td class="flex">
+                                <button class="btn btn-square btn-sm text-primary mr-2 edit-customer-btn"
+                                        data-customer-id="${customer.customerId}">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                         stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                              d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"/>
+                                    </svg>
+                                </button>
+                                <button class="btn btn-square btn-sm text-primary">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                         stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                              d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"/>
+                                    </svg>
+                                </button>
+                            </td>  
+                        </tr>`;
+                $('#customer_table tbody').append(rowHtml);
+            });
+        },
+        function (err) {
+            console.error('Error loading customer:', err);
+        })
+}
 
-    function loadAllCustomers(){
+loadAllCustomers();
 
+// Event delegation for dynamically added button
+$('#saveBtn').on('click', function () {
+    let nameVal = customerName.val().trim();
+    let addressVal = customerAddress.val().trim();
+    let contactVal = customerContact.val().trim();
+    let emailVal = customerEmail.val().trim();
+    let dobVal = customerDob.val();
+    let genderVal = $('input[name="customer_gender"]:checked').length > 0 ? $('input[name="customer_gender"]:checked').attr('aria-label') : '';
+
+    removeCustomerValidationError();
+
+    // Validation checks
+    let errors = [];
+
+    if (!validator.isLength(nameVal, {min: 2, max: 50})) {
+        errors.push('Name must be between 2 and 50 characters');
+        $('#customer_name_error').text('Name must be between 2 and 50 characters');
+        customerName.addClass('input-error');
+    }
+    if (!validator.isEmail(emailVal)) {
+        errors.push('Invalid email format');
+        $('#customer_email_error').text('Invalid email format');
+        customerEmail.addClass('input-error');
+    }
+    if (!validator.isLength(addressVal, {min: 5, max: 100})) {
+        errors.push('Address must be between 5 and 100 characters');
+        $('#customer_address_error').text('Address must be between 5 and 100 characters');
+        customerAddress.addClass('input-error');
+    }
+    if (!validator.isMobilePhone(contactVal, 'any', {strictMode: false})) {
+        errors.push('Invalid contact number');
+        $('#customer_contact_error').text('Invalid contact number');
+        customerContact.addClass('input-error');
+    }
+    if (!validator.isDate(dobVal)) {
+        errors.push('Invalid dob');
+        $('#customer_dob_error').text('Invalid dob');
+        customerDob.addClass('input-error');
+    }
+    if (validator.isEmpty(genderVal)) {
+        errors.push('Please select gender');
+        $('#customer_gender_error').text('Please select gender');
     }
 
-    // Event delegation for dynamically added button
-    $('#saveBtn').on('click', function() {
-        let nameVal = customerName.val().trim();
-        let addressVal = customerAddress.val().trim();
-        let contactVal = customerContact.val().trim();
-        let emailVal = customerEmail.val().trim();
-        let dobVal = customerDob.val();
-        let genderVal = $('input[name="customer_gender"]:checked').length > 0 ? $('input[name="customer_gender"]:checked').attr('aria-label') : '';
-
-        removeCustomerValidationError();
-
-        // Validation checks
-        let errors = [];
-
-        if (!validator.isLength(nameVal, { min: 2, max: 50 })) {
-            errors.push('Name must be between 2 and 50 characters');
-            $('#customer_name_error').text('Name must be between 2 and 50 characters');
-            customerName.addClass('input-error');
-        }
-        if (!validator.isEmail(emailVal)) {
-            errors.push('Invalid email format');
-            $('#customer_email_error').text('Invalid email format');
-            customerEmail.addClass('input-error');
-        }
-        if (!validator.isLength(addressVal, { min: 5, max: 100 })) {
-            errors.push('Address must be between 5 and 100 characters');
-            $('#customer_address_error').text('Address must be between 5 and 100 characters');
-            customerAddress.addClass('input-error');
-        }
-        if (!validator.isMobilePhone(contactVal, 'any', { strictMode: false })) {
-            errors.push('Invalid contact number');
-            $('#customer_contact_error').text('Invalid contact number');
-            customerContact.addClass('input-error');
-        }
-        if (!validator.isDate(dobVal)){
-            errors.push('Invalid dob');
-            $('#customer_dob_error').text('Invalid dob');
-            customerDob.addClass('input-error');
-        }
-        if (validator.isEmpty(genderVal)){
-            errors.push('Please select gender');
-            $('#customer_gender_error').text('Please select gender');
-        }
-
-        if (errors.length > 0) {
-            return;
-        }
-
-        const customer = new CustomerModel(nameVal, genderVal.toUpperCase(), dobVal, addressVal, contactVal, emailVal);
-
-        saveCustomer(customer,
-            function() {
-                console.log('Customer saved:');
-            },
-            function(error) {
-                console.error('Error saving customer:', error);
-            }
-        );
-    });
-
-    function removeCustomerValidationError(){
-        const inputs = [customerName, customerAddress, customerContact, customerEmail, customerEmail, customerDob];
-        const errorLabels = [$('#customer_name_error'), $('#customer_email_error'), $('#customer_address_error'), $('#customer_contact_error'), $('#customer_dob_error'), $('#customer_gender_error')];
-
-        inputs.forEach(input => input.removeClass('input-error'));
-        errorLabels.forEach(errorLabel => errorLabel.text(''));
+    if (errors.length > 0) {
+        return;
     }
+
+    const customer = new CustomerModel(nameVal, genderVal.toUpperCase(), dobVal, addressVal, contactVal, emailVal);
+
+    saveCustomer(customer,
+        function () {
+            loadAllCustomers();
+        },
+        function (error) {
+            console.error('Error saving customer:', error);
+        }
+    );
 });
+
+function editCustomerModal(customerId) {
+
+    getCustomerById(customerId,
+        function (customer) {
+            new_customer_form.showModal();
+
+            customerName.val(customer.name);
+            customerEmail.val(customer.email);
+            customerContact.val(customer.contact);
+            customerAddress.val(customer.address);
+        },
+        function (error) {
+            console.error('Error saving customer:', error);
+        }
+    );
+}
+
+$(document).on('click', '.edit-customer-btn', function () {
+    let customerId = $(this).attr('data-customer-id');
+    editCustomerModal(customerId);
+});
+
+function removeCustomerValidationError() {
+    const inputs = [customerName, customerAddress, customerContact, customerEmail, customerEmail, customerDob];
+    const errorLabels = [$('#customer_name_error'), $('#customer_email_error'), $('#customer_address_error'), $('#customer_contact_error'), $('#customer_dob_error'), $('#customer_gender_error')];
+
+    inputs.forEach(input => input.removeClass('input-error'));
+    errorLabels.forEach(errorLabel => errorLabel.text(''));
+}
+
+
+$(document).on('click', '#add_customer_btn',function() {
+    customerName.val('');
+    customerEmail.val('');
+    customerContact.val('');
+    customerAddress.val('');
+
+    new_customer_form.showModal()
+})
 
 
 
