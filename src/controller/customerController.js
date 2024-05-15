414cd93f0fa1b71bd8b4292dd5074ee23544e945
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-05-15 18:58:49 +0530

    feat: toast

--- src/controller/customerController.js
+++ src/controller/customerController.js
@@ -1,6 +1,7 @@
 import validator from "validator/es";
 import CustomerModel from "../model/CustomerModel.js";
 import {getAllCustomers, getCustomerById, saveCustomer, updateCustomer} from "../api/Customer.js";
+import {showToast} from "../util/toast.js";
 
 const customerName = $('#customer_name');
 const customerAddress = $('#customer_address');
@@ -32,7 +33,8 @@ function loadAllCustomers() {
                                               d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"/>
                                     </svg>
                                 </button>
-                                <button class="btn btn-square btn-sm text-primary">
+                                <button class="btn btn-square btn-sm text-primary delete-customer-btn"
+                                        data-customer-id="${customer.customerId}">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                          stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                         <path stroke-linecap="round" stroke-linejoin="round"
@@ -116,6 +118,7 @@ $('#customerSaveBtn').on('click', function () {
             function () {
                 loadAllCustomers();
                 new_customer_form.close();
+                startWindToast('Customer Updated', 'customer updated successfully', 'error', 3, 'right')
             },
             function (error) {
                 console.error('Error updating customer:', error);
@@ -151,6 +154,11 @@ $(document).on('click', '.edit-customer-btn', function () {
     changeToEditCustomerModal(customerId);
 });
 
+$(document).on('click', '.delete-customer-btn', function () {
+    let customerId = $(this).attr('data-customer-id');
+    showToast('success','This is a bottom right toast!');
+});
+
 function removeCustomerValidationError() {
     const inputs = [customerName, customerAddress, customerContact, customerEmail, customerEmail, customerDob];
     const errorLabels = [$('#customer_name_error'), $('#customer_email_error'), $('#customer_address_error'), $('#customer_contact_error'), $('#customer_dob_error'), $('#customer_gender_error')];
@@ -171,7 +179,8 @@ $(document).on('click', '#add_customer_btn',function() {
     customerDob.val('');
 
     new_customer_form.showModal()
-})
+});
+
 
 
 
---
+++ src/util/toast.js
@@ -0,0 +1,38 @@
+export function showToast(type, message) {
+    const toast = $('#toast');
+
+    if (type) {
+        const successAlert = `
+                <div role="alert" class="alert flex gap-3" style="color: #10B981;">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6 mr-3" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                    <span>${message}</span>
+                </div>
+            `;
+        const errorAlert = `
+                <div role="alert" class="alert flex gap-3" style="color: #b9102f;">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                  <span>${message}</span>
+                </div>
+            `;
+        const infoAlert = `
+                <div role="alert" class="alert flex gap-3" style="color: #10b9ab;">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                  <span>${message}</span>
+                </div>
+            `;
+
+        if (type === 'success'){
+            toast.html(successAlert);
+        }else if (type === 'error'){
+            toast.html(errorAlert);
+        }else if (type === 'info'){
+            toast.html(infoAlert);
+        }
+        toast.removeClass('hidden');
+
+        // Hide the toast after timeout milliseconds
+        setTimeout(() => {
+            toast.addClass('hidden');
+        }, 3000);
+    }
+}
