c7cad15b7b18ce2421b0ca1f61717c34a14682c6
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-05-17 04:19:13 +0530

    few changes

--- src/controller/customerController.js
+++ src/controller/customerController.js
@@ -73,27 +73,22 @@ $('#customerSaveBtn').on('click', function () {
     if (!validator.isLength(nameVal, {min: 2, max: 50})) {
         errors.push('Name must be between 2 and 50 characters');
         $('#customer_name_error').text('Name must be between 2 and 50 characters');
-        customerName.addClass('input-error');
     }
     if (!validator.isEmail(emailVal)) {
         errors.push('Invalid email format');
         $('#customer_email_error').text('Invalid email format');
-        customerEmail.addClass('input-error');
     }
     if (!validator.isLength(addressVal, {min: 5, max: 100})) {
         errors.push('Address must be between 5 and 100 characters');
         $('#customer_address_error').text('Address must be between 5 and 100 characters');
-        customerAddress.addClass('input-error');
     }
     if (!validator.isMobilePhone(contactVal, 'any', {strictMode: false})) {
         errors.push('Invalid contact number');
         $('#customer_contact_error').text('Invalid contact number');
-        customerContact.addClass('input-error');
     }
     if (!validator.isDate(dobVal)) {
         errors.push('Invalid dob');
         $('#customer_dob_error').text('Invalid dob');
-        customerDob.addClass('input-error');
     }
     if (validator.isEmpty(genderVal)) {
         errors.push('Please select gender');
@@ -184,10 +179,8 @@ $(document).on('click', '.delete-customer-btn', function () {
 });
 
 function removeCustomerValidationError() {
-    const inputs = [customerName, customerAddress, customerContact, customerEmail, customerEmail, customerDob];
     const errorLabels = [$('#customer_name_error'), $('#customer_email_error'), $('#customer_address_error'), $('#customer_contact_error'), $('#customer_dob_error'), $('#customer_gender_error')];
 
-    inputs.forEach(input => input.removeClass('input-error'));
     errorLabels.forEach(errorLabel => errorLabel.text(''));
 }
 
