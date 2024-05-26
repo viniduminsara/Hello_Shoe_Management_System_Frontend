625dc08988931bd07fa6f200893768a4aad8aff1
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-05-26 20:00:04 +0530

    feat: load chart data

--- src/controller/dashboardController.js
+++ src/controller/dashboardController.js
@@ -1,70 +1,81 @@
-function loadChart() {
-    var options = {
-        series: [{
-            name: 'Sales',
-            data: [4, 3, 10, 9, 29, 19, 22, 9, 12, 7, 19, 5, 13, 9, 17, 2, 7, 5]
-        }],
-        chart: {
-            height: 350,
-            type: 'line',
-        },
-        forecastDataPoints: {
-            count: 7
-        },
+export function loadChart(verityTypeQuantities) {
+    const series = [];
+    const labels = [];
+    for (const verityTypeQty of verityTypeQuantities){
+        series.push(verityTypeQty.quantity);
+        labels.push(verityTypeQty.verityType);
+    }
+
+    const options = {
+        series: series,
+        labels: labels,
         stroke: {
-            width: 5,
-            curve: 'smooth'
+            show: false,
         },
-        xaxis: {
-            type: 'datetime',
-            categories: ['1/11/2000', '2/11/2000', '3/11/2000', '4/11/2000', '5/11/2000', '6/11/2000', '7/11/2000', '8/11/2000', '9/11/2000', '10/11/2000', '11/11/2000', '12/11/2000', '1/11/2001', '2/11/2001', '3/11/2001','4/11/2001' ,'5/11/2001' ,'6/11/2001'],
-            tickAmount: 10,
-            labels: {
-                formatter: function(value, timestamp, opts) {
-                    return opts.dateFormatter(new Date(timestamp), 'dd MMM')
+        chart: {
+            type: 'donut',
+            width: 400,
+            height: 400,
+        },
+        responsive: [
+            {
+                breakpoint: 1024,
+                options: {
+                    chart: {
+                        width: '80%',
+                        height: 350
+                    },
+                    legend: {
+                        position: 'right'
+                    }
+                }
+            },
+            {
+                breakpoint: 768,
+                options: {
+                    chart: {
+                        width: '90%',
+                        height: 300
+                    },
+                    legend: {
+                        position: 'bottom'
+                    }
+                }
+            },
+            {
+                breakpoint: 480,
+                options: {
+                    chart: {
+                        width: '100%',
+                        height: 250
+                    },
+                    legend: {
+                        position: 'bottom'
+                    }
                 }
             }
-        },
-        title: {
-            text: 'Sale Details',
-            align: 'left',
-            style: {
-                fontSize: "24px",
-                color: '#687681'
-            }
-        },
-        fill: {
-            type: 'gradient',
-            gradient: {
-                shade: 'dark',
-                gradientToColors: [ '#00CDB8'],
-                shadeIntensity: 1,
-                type: 'horizontal',
-                opacityFrom: 1,
-                opacityTo: 1,
-                stops: [0, 100, 100, 100]
-            },
-        }
+        ]
     };
 
-    let chart = new ApexCharts(document.querySelector("#chart"), options);
+    const chart = new ApexCharts(document.querySelector("#chart"), options);
     chart.render();
+
 }
 
-function toggleTheme() {
-    const currentTheme = document.documentElement.getAttribute('data-theme');
-    const newTheme = currentTheme === 'sunset' ? 'winter' : 'sunset';
+let currentTheme = document.documentElement.getAttribute('data-theme');
+
+$('#theme_changer').click(function () {
+    let newTheme = currentTheme === 'sunset' ? 'winter' : 'sunset';
     document.documentElement.setAttribute('data-theme', newTheme);
+    currentTheme = newTheme;
 
-    const moonIcon = document.getElementById('moon');
-    const sunIcon = document.getElementById('sun');
+    const moonIcon = $('#moon');
+    const sunIcon = $('#sun');
     if (currentTheme === 'sunset') {
-        moonIcon.style.display = 'none';
-        sunIcon.style.display = 'block';
+        moonIcon.hide();
+        sunIcon.show();
     } else {
-        moonIcon.style.display = 'block';
-        sunIcon.style.display = 'none';
+        moonIcon.show();
+        sunIcon.hide();
     }
-}
-
-loadChart();
+});
